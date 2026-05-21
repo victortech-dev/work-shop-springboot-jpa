43ad914d9581c8a62572b1eaa9bfe983800dd8a4
vkdev <dev.victorkaua@gmail.com>
2026-05-21 13:44:11 -0300

    Camada de servico, resgistro de componentes

---
+++ src/main/java/com/konezocorp/course/services/UserService.java
@@ -0,0 +1,32 @@
+package com.konezocorp.course.services;
+
+import com.konezocorp.course.entities.User;
+import com.konezocorp.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+
+        return repository.findAll();
+
+    }
+
+    public User findById(Long id) {
+
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+
+    }
+
+
+}
