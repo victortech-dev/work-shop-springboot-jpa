d39f15dbfb8c4c0f0619493be1cd2915846daae4
vkdev <dev.victorkaua@gmail.com>
2026-05-20 21:00:20 -0300

    H2 database, test profile , JPA

--- src/main/java/com/konezocorp/course/entities/User.java
+++ src/main/java/com/konezocorp/course/entities/User.java
@@ -1,11 +1,16 @@
 package com.konezocorp.course.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
-    private static final long Serializable =1L;
-
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String phone;
