ba3c5bb6dfd3fdff2a11eae22c8e98c3bee46fa2
vkdev <dev.victorkaua@gmail.com>
2026-05-25 12:28:41 -0300

    Product

--- src/main/java/com/konezocorp/course/entities/Category.java
+++ src/main/java/com/konezocorp/course/entities/Category.java
@@ -3,7 +3,10 @@ package com.konezocorp.course.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
+
 @Entity
 @Table(name = "tb_category")
 
@@ -17,7 +20,11 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
-    public Category(){}
+    @Transient
+    private Set<Product> products = new HashSet<>();
+
+    public Category() {
+    }
 
     public Category(Long id, String name) {
         this.id = id;
@@ -50,5 +57,9 @@ public class Category implements Serializable {
     @Override
     public int hashCode() {
         return Objects.hashCode(id);
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 }
