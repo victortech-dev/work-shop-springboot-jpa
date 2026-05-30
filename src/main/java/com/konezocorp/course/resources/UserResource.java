61cf1c545330b597e9be184bdb2db7a1f431b759
vkdev <dev.victorkaua@gmail.com>
2026-05-29 22:11:37 -0300

    User Delete

--- src/main/java/com/konezocorp/course/resources/UserResource.java
+++ src/main/java/com/konezocorp/course/resources/UserResource.java
@@ -29,8 +29,15 @@ public class UserResource {
     @PostMapping
     public ResponseEntity<User> insert(@RequestBody User obj){
         obj = service.insert(obj);
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{/id}").buildAndExpand(obj.getId()).toUri();
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+            service.delete(id);
+            return ResponseEntity.noContent().build();
+    }
+
 
 }
