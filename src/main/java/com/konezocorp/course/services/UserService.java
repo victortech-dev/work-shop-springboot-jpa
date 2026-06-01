79b7dad8f6b061e5895259168438e3e1b2f28484
vkdev <dev.victorkaua@gmail.com>
2026-06-01 11:45:59 -0300

    Tratamento de exception findById

--- src/main/java/com/konezocorp/course/services/UserService.java
+++ src/main/java/com/konezocorp/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.konezocorp.course.services;
 
 import com.konezocorp.course.entities.User;
 import com.konezocorp.course.repositories.UserRepository;
+import com.konezocorp.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Service;
@@ -24,7 +25,7 @@ public class UserService {
     public User findById(Long id) {
 
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 
     }
     public User insert(User obj){
