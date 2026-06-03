ba5308fa9ce82a18a55d068580751a01c07f2d3c
vkdev <dev.victorkaua@gmail.com>
2026-06-02 22:33:11 -0300

    Tratamento de exceção -Update

--- src/main/java/com/konezocorp/course/services/UserService.java
+++ src/main/java/com/konezocorp/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.konezocorp.course.entities.User;
 import com.konezocorp.course.repositories.UserRepository;
 import com.konezocorp.course.services.exceptions.DatabaseException;
 import com.konezocorp.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
@@ -49,9 +50,15 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }catch(EntityNotFoundException e ) {
+            throw new ResourceNotFoundException(id);
+
+        }
+
     }
 
     private void updateData(User entity, User obj) {
